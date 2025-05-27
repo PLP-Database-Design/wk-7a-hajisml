0d7ec3796159505c2e2310cb4d2753fae44e62ff
Haji Ibrahim <hajisml@outlook.com>
2025-05-27 16:23:18 +0300

    Q1

---
+++ answers.sql
@@ -0,0 +1,14 @@
+-- Question One
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
